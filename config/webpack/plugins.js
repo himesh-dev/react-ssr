eeda6d75fcd73eaaebf5e927d0f08ef87b6c1418
himesh-dev <askhimesh1@gmail.com>
2021-05-14 17:42:26 +0530

    miniCssExtractPlugin was adding linktag in html so added htmlInlineCsswebpackPlugin for adding styles in head

--- config/webpack/plugins.js
+++ config/webpack/plugins.js
@@ -1,8 +1,16 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HTMLInlineCSSWebpackPlugin =
+  require('html-inline-css-webpack-plugin').default;
 const path = require('path');
+
 module.exports = [
+  new MiniCssExtractPlugin({
+    // filename: 'styles/style.css',
+  }),
   new HtmlWebpackPlugin({
     template: path.resolve(process.cwd(), 'public/index.html'),
     inject: 'body',
   }),
+  new HTMLInlineCSSWebpackPlugin(),
 ];
