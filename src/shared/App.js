b3659f3db92218be4eb0c14be542a3d64a17b048
himesh-dev <askhimesh1@gmail.com>
2021-05-16 03:30:06 +0530

    image working

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles.css';
+import image from '../assets/imageIcon.png';
 
 const App = () => {
   const clickHandler = () => {
@@ -10,6 +11,7 @@ const App = () => {
       <button onClick={clickHandler} className="button">
         clickme128
       </button>
+      <img src={image} height={100} widht={'auto'} alt="expired" />
     </div>
   );
 };
